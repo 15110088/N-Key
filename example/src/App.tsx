08f2272d983df88a1af5365c78903704b64af934
lnghia18l@gmail.com <lnghia18l@gmail.com>
2021-11-09 22:27:22 +0700

    add

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 
 import { StyleSheet, View, Text, Button } from 'react-native';
 import { multiply,add, GetNKey, nghiaText, GetSDE,GetToken, GetIIS } from 'nkey';
-
+//new viewe sion
 export default function App() {
   const [result, setResult] = React.useState<number | undefined>();
 
