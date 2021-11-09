da70e9909c63bcc8c83cbd7ba9a8569578f5218c
lnghia18l@gmail.com <lnghia18l@gmail.com>
2021-11-09 16:52:02 +0700

    add token

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,21 +1,23 @@
 import * as React from 'react';
 
-import { StyleSheet, View, Text } from 'react-native';
-import { multiply,add, GetNKey, nghiaText } from 'nkey';
+import { StyleSheet, View, Text, Button } from 'react-native';
+import { multiply,add, GetNKey, nghiaText, GetSDE,GetToken, GetIIS } from 'nkey';
 
 export default function App() {
   const [result, setResult] = React.useState<number | undefined>();
 
   React.useEffect(() => {
     add(3, 7).then(setResult);
-    GetNKey().then(req=>console.log(req));
+    GetToken().then(req=>console.log(req));
 
   }, []);
 
   return (
     <View style={styles.container}>
       <Text>Result: {result}</Text>
-      
+      <Button title="123" onPress={()=>GetSDE().then(req=>console.log(req))}></Button>
+      <Button title="123" onPress={()=>GetIIS().then(req=>console.log(req))}></Button>
+
       {nghiaText()}
     </View>
   );
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,10 @@
 import React from "react";
 import { Text } from "react-native";
 var CryptoJS = require("crypto-js");
+const axios = require('axios');
 
-
+const host="https://datdai.stnmt.dongnai.gov.vn:8443/apidatdai2021Qh"
+//const host="http://192.169.3.197/Apidatdai"
 
 export function multiply(a: number, b: number): Promise<number> {
   return Promise.resolve(a * b);
@@ -15,18 +17,42 @@ export function add(a: number, b: number): Promise<number> {
 export const  GetNKey =async()=>{
   const key = 'KthdnptcttcnttSTNMT@lovev158942y'
   const iv = 'GDEX1you!fgetTST'
-  const cipher = CryptoJS.AES.encrypt('masteryou-'+Date.now(), CryptoJS.enc.Utf8.parse(key), {
+  const cipher = CryptoJS.AES.encrypt('masteryou-'+Math.floor(Date.now() /1000) , CryptoJS.enc.Utf8.parse(key), {
       iv: CryptoJS.enc.Utf8.parse(iv),
       padding: CryptoJS.pad.Pkcs7,
       mode: CryptoJS.mode.CBC
   })
-
-
-
+  console.log(cipher.ciphertext.toString(CryptoJS.enc.Base64))
 
   return cipher.ciphertext.toString(CryptoJS.enc.Base64);
-
 }
+
+export const  GetSDE =async()=>
+{
+    const keyhash= await GetNKey();
+    const data= await axios.post(host+'/api/SDE/GET',{hash:keyhash}) 
+    return data.data;
+}
+
+
+export const  GetToken =async()=>{
+  const key = 'KthdnptcttcnttSTNMT@lovev158942y'
+  const iv = 'GDEX1you!fgetTST'
+  const cipher = CryptoJS.AES.encrypt('masteryou-'+Math.floor(Date.now() /1000) , CryptoJS.enc.Utf8.parse(key), {
+      iv: CryptoJS.enc.Utf8.parse(iv),
+      padding: CryptoJS.pad.Pkcs7,
+      mode: CryptoJS.mode.CBC
+  })
+  return cipher.ciphertext.toString(CryptoJS.enc.Base64);
+}
+
+
+export const  GetIIS =async()=>{
+  const keyhash= await GetNKey();
+   const data= await axios.post(host+'/api/SDE/GetByID',{hash:keyhash}) 
+    return data.data;
+}
+
 
 export const nghiaText=()=>(
 
